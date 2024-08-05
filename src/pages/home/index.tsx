5eff346b3d97abf41b4ef29e45373a57bf43cd6e
anatadic <anatadic111@gmail.com>
2024-08-05 12:44:11 +0200

    added search input, search queries & handle search function

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,25 +1,61 @@
-import { useState } from 'react';
-import { Button } from 'semantic-ui-react';
+import { useEffect, useState } from 'react';
+import { Button, Input } from 'semantic-ui-react';
 import { ColumnDisplay } from './column-display';
 
-import { fetchMovies, fetchTVShows } from './query';
-import { useQuery } from '@tanstack/react-query';
+import {
+  fetchMovies,
+  fetchTVShows,
+  searchMovies,
+  searchTVShows,
+} from './query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { Navigate } from 'react-router-dom';
-import { DisplayType } from './types';
+import { DisplayData, DisplayType } from './types';
 
 export const Home = () => {
   const [displayType, setDisplayType] = useState<DisplayType>(
     DisplayType.Movies
   );
+  const [searchItem, setSearchItem] = useState('');
+  const [movies, setMovies] = useState<DisplayData[]>([]);
+  const [tvShows, setTvShows] = useState<DisplayData[]>([]);
 
   const { data: movieData, isLoading: isLoadingMovies } = useQuery({
     queryKey: ['movies'],
     queryFn: fetchMovies,
   });
+
   const { data: tvShowData, isLoading: isLoadingTvShows } = useQuery({
     queryKey: ['tvshows'],
     queryFn: fetchTVShows,
   });
+
+  const { mutate: searchMovieMutation, data: searchMovieData } = useMutation({
+    mutationKey: ['searchMovie'],
+    mutationFn: () => searchMovies(searchItem),
+  });
+
+  const { mutate: searchTvShowMutation, data: searchTvShowData } = useMutation({
+    mutationKey: ['searchTVShow'],
+    mutationFn: () => searchTVShows(searchItem),
+  });
+
+  const handleSearch = async () => {
+    displayType === DisplayType.Movies
+      ? searchMovieMutation()
+      : searchTvShowMutation();
+
+    displayType === DisplayType.Movies
+      ? setMovies(searchMovieData?.results)
+      : setTvShows(searchTvShowData?.results);
+  };
+
+  useEffect(() => {
+    if (!searchItem) {
+      setMovies(movieData?.results);
+      setTvShows(tvShowData?.results);
+    }
+  }, [searchItem, movieData, tvShowData]);
 
   if (localStorage.getItem('guest_session_id') === null) {
     return <Navigate to="/auth" />;
@@ -27,34 +63,54 @@ export const Home = () => {
 
   return (
     <div style={{ marginTop: 50, height: 'auto' }}>
-      <Button.Group>
-        <Button
-          color={displayType === DisplayType.Movies ? 'blue' : undefined}
-          onClick={() => setDisplayType(DisplayType.Movies)}
-        >
-          Movies
-        </Button>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <div>
+          <Input
+            type="text"
+            icon={'search'}
+            value={searchItem}
+            onChange={(e) => setSearchItem(e.target.value)}
+            placeholder="Type to search"
+            action={{ onClick: () => handleSearch() }}
+          />
+        </div>
 
-        <Button
-          color={displayType === DisplayType.TVShows ? 'blue' : undefined}
-          onClick={() => setDisplayType(DisplayType.TVShows)}
-        >
-          TV Shows
-        </Button>
-      </Button.Group>
+        <Button.Group>
+          <Button
+            color={displayType === DisplayType.Movies ? 'blue' : undefined}
+            onClick={() => setDisplayType(DisplayType.Movies)}
+          >
+            Movies
+          </Button>
+
+          <Button
+            color={displayType === DisplayType.TVShows ? 'blue' : undefined}
+            onClick={() => setDisplayType(DisplayType.TVShows)}
+          >
+            TV Shows
+          </Button>
+        </Button.Group>
+        <div></div>
+      </div>
       {isLoadingMovies || isLoadingTvShows ? (
         <div> Loading... </div>
       ) : (
         <div style={{ marginTop: 20 }}>
           {displayType === DisplayType.Movies ? (
             <ColumnDisplay
-              data={movieData.results}
+              data={movies}
               displayType={DisplayType.Movies}
               isRated={false}
             />
           ) : (
             <ColumnDisplay
-              data={tvShowData.results}
+              data={tvShows}
               displayType={DisplayType.TVShows}
               isRated={false}
             />
--- src/pages/home/query.ts
+++ src/pages/home/query.ts
@@ -23,3 +23,29 @@ export const fetchTVShows = async () => {
 
   return res.json();
 };
+
+export const searchMovies = async (query: string) => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`,
+    {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
+      },
+    }
+  );
+
+  return res.json();
+};
+
+export const searchTVShows = async (query: string) => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/tv?query=${query}&include_adult=false&language=en-US&page=1`,
+    {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
+      },
+    }
+  );
+
+  return res.json();
+};
