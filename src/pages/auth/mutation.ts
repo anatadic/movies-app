5b12e09d1518667b3f848923b4aad3ffa4508c13
anatadic <anatadic111@gmail.com>
2024-08-15 14:52:32 +0200

    test

--- src/pages/auth/mutation.ts
+++ src/pages/auth/mutation.ts
@@ -3,7 +3,7 @@ export const mutationLogin = async () => {
     'https://api.themoviedb.org/3/authentication/guest_session/new',
     {
       headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYzNhNmYzNGNmNTA3ODE1MzY1ZjNiNjg1MmE4MGU0MyIsIm5iZiI6MTcyMjI1MzMxNS41MDI2OTQsInN1YiI6IjY2YTc3N2U1YjhhOThhMzQ3Y2E1M2E1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gZdgyrzvEhDakQ2kWnCCP9wEkdq4KVzT_-tFKN2lKN8`,
       },
     }
   );
